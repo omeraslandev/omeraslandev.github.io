4b53c7b2404d93e9ece5c6fd1a004e2fa97b5f4f
ömer <allahdiyenayatokirishima@gmail.com>
2022-11-26 09:33:46 +0300

    random background was has been slain.

--- assets/js/random.js
+++
@@ -1,6 +0,0 @@
-var backgrounds = new Array("background-1.gif", "background-2.gif", "background-3.gif");
-var random_int = Math.floor(Math.random() * 3)
-
-console.log("url(assets/img/"+backgrounds[random_int]+".gif)")
-
-document.getElementById('hero').style.backgroundImage = "url(assets/img/"+backgrounds[random_int];
